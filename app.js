2d22a034ade9ceb9ab114b285f6381c0e74e580b
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-02-18 12:14:39 +0600

    responsive

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.getElementById('calculate-button').addEventListener('click',function(){
+    
    
    const incomeInput=document.getElementById('income-input');
    const inputAmountText=incomeInput.value;
@@ -8,9 +9,55 @@ document.getElementById('calculate-button').addEventListener('click',function(){
     const incomeTotal=document.getElementById('balance-total');
     const incomeTotaltext=incomeTotal.innerText;
     const previousIncomeTotal=parseFloat(incomeTotaltext);
-    incomeTotal.innerText=previousIncomeTotal+incomeAmount;
+    const incomeTotalTotal=previousIncomeTotal+incomeAmount;
+    incomeTotal.innerText=incomeTotalTotal;
     incomeInput.value='';
-})
+     
+
+    document.getElementById('save-btn').addEventListener('click',function(){
+
+        
+              const costInput=document.getElementById('percent-input');
+          const inputCostText=costInput.value;
+          const costAmount=parseFloat(inputCostText);
+        
+     //    current income
+          const costTotal=document.getElementById('saving-amount');
+          const costTotaltext=costTotal.innerText;
+          let  previouscostTotal=parseFloat(costTotaltext);
+          let svingPercent=costAmount/100;
+          let savings=svingPercent*incomeTotalTotal;
+          let totalParcent=previouscostTotal+savings;
+          costTotal.innerText=totalParcent;
+     
+     
+         costInput.value='';
+
+         
+         
+     
+     
+     
+        //  .valuincomeInpute='';
+         
+        const rcostInput=document.getElementById('remaining-total');
+        const rinputCostText=rcostInput.value;
+        const rcostAmount=parseFloat(rinputCostText);
+        
+     //    current income
+         const rcostTotal=document.getElementById('balance-total');
+         const rcostTotaltext=rcostTotal.innerText;
+         const rpreviouscostTotal=parseFloat(rcostTotaltext);
+         rcostTotal.innerText=rpreviouscostTotal+rcostAmount;
+         rcostInput.value='';
+     
+     
+         
+     })
+    
+   
+})  
+
 
     // expensess?
     document.getElementById('calculate-button').addEventListener('click',function(){
@@ -44,26 +91,34 @@ document.getElementById('calculate-button').addEventListener('click',function(){
    const inputCostText=costInput.value;
    const costAmount=parseFloat(inputCostText);
    
+   
 //    current income
     const costTotal=document.getElementById('expenses-total');
     const costTotaltext=costTotal.innerText;
     const previouscostTotal=parseFloat(costTotaltext);
     let totalCost=previouscostTotal+costAmount;
     costTotal.innerText=totalCost;
+    costInput.value='';
+
 
     // update balance
     const balanceTotal=document.getElementById('balance-total');
     const balanceTotaltext=balanceTotal.innerText;
     const previousBalanceTotal=parseFloat(balanceTotaltext);
-    balanceTotal.innerText=previousBalanceTotal-totalCost;
+    let subTotalBalance=previousBalanceTotal-totalCost;
+    balanceTotal.innerText=subTotalBalance;
+    
+    
 
+    
+        
 
-
-    costInput.value='';
-
+    
 
     })
     
+   
+
 
     
 
